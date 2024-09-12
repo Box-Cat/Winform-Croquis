b67bf4b478867610bd4d88c7d9fecc75ead95dff
Box-Cat <pjh211pjh@gmail.com>
2024-09-13 00:22:21 +0900

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,426 @@
+﻿namespace Croquis
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            tableLayoutPanelForm = new TableLayoutPanel();
+            tableLayoutPanelPicBox = new TableLayoutPanel();
+            panelPicBox = new Panel();
+            picBox = new PictureBox();
+            tableLayoutPanelLeftBtn = new TableLayoutPanel();
+            btnLeft = new Button();
+            panelCheckBox = new Panel();
+            label3 = new Label();
+            checkBoxSize = new CheckBox();
+            tableLayoutPanelRightBtn = new TableLayoutPanel();
+            btnRight = new Button();
+            tableLayoutPanelBtns = new TableLayoutPanel();
+            panelTimer = new Panel();
+            label2 = new Label();
+            label1 = new Label();
+            textBoxSec = new TextBox();
+            textBoxMin = new TextBox();
+            textBoxHour = new TextBox();
+            panelStartBtn = new Panel();
+            btnStart = new Button();
+            panelPause = new Panel();
+            btnPause = new Button();
+            panelStop = new Panel();
+            btnStop = new Button();
+            menuStrip = new MenuStrip();
+            loadToolStripMenuItem = new ToolStripMenuItem();
+            timer = new System.Windows.Forms.Timer(components);
+            tableLayoutPanelForm.SuspendLayout();
+            tableLayoutPanelPicBox.SuspendLayout();
+            panelPicBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)picBox).BeginInit();
+            tableLayoutPanelLeftBtn.SuspendLayout();
+            panelCheckBox.SuspendLayout();
+            tableLayoutPanelRightBtn.SuspendLayout();
+            tableLayoutPanelBtns.SuspendLayout();
+            panelTimer.SuspendLayout();
+            panelStartBtn.SuspendLayout();
+            panelPause.SuspendLayout();
+            panelStop.SuspendLayout();
+            menuStrip.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanelForm
+            // 
+            tableLayoutPanelForm.ColumnCount = 1;
+            tableLayoutPanelForm.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanelForm.Controls.Add(tableLayoutPanelPicBox, 0, 0);
+            tableLayoutPanelForm.Controls.Add(tableLayoutPanelBtns, 0, 1);
+            tableLayoutPanelForm.Dock = DockStyle.Fill;
+            tableLayoutPanelForm.Location = new Point(0, 24);
+            tableLayoutPanelForm.Name = "tableLayoutPanelForm";
+            tableLayoutPanelForm.RowCount = 2;
+            tableLayoutPanelForm.RowStyles.Add(new RowStyle(SizeType.Percent, 90F));
+            tableLayoutPanelForm.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+            tableLayoutPanelForm.Size = new Size(800, 426);
+            tableLayoutPanelForm.TabIndex = 0;
+            // 
+            // tableLayoutPanelPicBox
+            // 
+            tableLayoutPanelPicBox.ColumnCount = 3;
+            tableLayoutPanelPicBox.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 50F));
+            tableLayoutPanelPicBox.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanelPicBox.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 50F));
+            tableLayoutPanelPicBox.Controls.Add(panelPicBox, 1, 0);
+            tableLayoutPanelPicBox.Controls.Add(tableLayoutPanelLeftBtn, 0, 0);
+            tableLayoutPanelPicBox.Controls.Add(tableLayoutPanelRightBtn, 2, 0);
+            tableLayoutPanelPicBox.Dock = DockStyle.Fill;
+            tableLayoutPanelPicBox.Location = new Point(3, 3);
+            tableLayoutPanelPicBox.Name = "tableLayoutPanelPicBox";
+            tableLayoutPanelPicBox.RowCount = 1;
+            tableLayoutPanelPicBox.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanelPicBox.Size = new Size(794, 377);
+            tableLayoutPanelPicBox.TabIndex = 0;
+            // 
+            // panelPicBox
+            // 
+            panelPicBox.Controls.Add(picBox);
+            panelPicBox.Dock = DockStyle.Fill;
+            panelPicBox.Location = new Point(53, 3);
+            panelPicBox.Name = "panelPicBox";
+            panelPicBox.Size = new Size(688, 371);
+            panelPicBox.TabIndex = 2;
+            // 
+            // picBox
+            // 
+            picBox.Dock = DockStyle.Fill;
+            picBox.Location = new Point(0, 0);
+            picBox.Name = "picBox";
+            picBox.Size = new Size(688, 371);
+            picBox.TabIndex = 0;
+            picBox.TabStop = false;
+            // 
+            // tableLayoutPanelLeftBtn
+            // 
+            tableLayoutPanelLeftBtn.ColumnCount = 1;
+            tableLayoutPanelLeftBtn.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanelLeftBtn.Controls.Add(btnLeft, 0, 1);
+            tableLayoutPanelLeftBtn.Controls.Add(panelCheckBox, 0, 0);
+            tableLayoutPanelLeftBtn.Dock = DockStyle.Fill;
+            tableLayoutPanelLeftBtn.Location = new Point(3, 3);
+            tableLayoutPanelLeftBtn.Name = "tableLayoutPanelLeftBtn";
+            tableLayoutPanelLeftBtn.RowCount = 3;
+            tableLayoutPanelLeftBtn.RowStyles.Add(new RowStyle(SizeType.Percent, 46.6666679F));
+            tableLayoutPanelLeftBtn.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            tableLayoutPanelLeftBtn.RowStyles.Add(new RowStyle(SizeType.Percent, 53.3333359F));
+            tableLayoutPanelLeftBtn.Size = new Size(44, 371);
+            tableLayoutPanelLeftBtn.TabIndex = 3;
+            // 
+            // btnLeft
+            // 
+            btnLeft.Dock = DockStyle.Fill;
+            btnLeft.Location = new Point(3, 152);
+            btnLeft.Name = "btnLeft";
+            btnLeft.Size = new Size(38, 44);
+            btnLeft.TabIndex = 0;
+            btnLeft.Text = "◀";
+            btnLeft.UseVisualStyleBackColor = true;
+            btnLeft.Click += btnLeft_Click;
+            // 
+            // panelCheckBox
+            // 
+            panelCheckBox.Controls.Add(label3);
+            panelCheckBox.Controls.Add(checkBoxSize);
+            panelCheckBox.Dock = DockStyle.Fill;
+            panelCheckBox.Location = new Point(3, 3);
+            panelCheckBox.Name = "panelCheckBox";
+            panelCheckBox.Size = new Size(38, 143);
+            panelCheckBox.TabIndex = 1;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(1, 44);
+            label3.Name = "label3";
+            label3.Size = new Size(36, 15);
+            label3.TabIndex = 1;
+            label3.Text = "大/小";
+            // 
+            // checkBoxSize
+            // 
+            checkBoxSize.AutoSize = true;
+            checkBoxSize.Location = new Point(12, 71);
+            checkBoxSize.Name = "checkBoxSize";
+            checkBoxSize.Size = new Size(15, 14);
+            checkBoxSize.TabIndex = 0;
+            checkBoxSize.UseVisualStyleBackColor = true;
+            checkBoxSize.CheckedChanged += checkBoxSize_CheckedChanged;
+            // 
+            // tableLayoutPanelRightBtn
+            // 
+            tableLayoutPanelRightBtn.ColumnCount = 1;
+            tableLayoutPanelRightBtn.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanelRightBtn.Controls.Add(btnRight, 0, 1);
+            tableLayoutPanelRightBtn.Dock = DockStyle.Fill;
+            tableLayoutPanelRightBtn.Location = new Point(747, 3);
+            tableLayoutPanelRightBtn.Name = "tableLayoutPanelRightBtn";
+            tableLayoutPanelRightBtn.RowCount = 3;
+            tableLayoutPanelRightBtn.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanelRightBtn.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            tableLayoutPanelRightBtn.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanelRightBtn.Size = new Size(44, 371);
+            tableLayoutPanelRightBtn.TabIndex = 4;
+            // 
+            // btnRight
+            // 
+            btnRight.Dock = DockStyle.Fill;
+            btnRight.Location = new Point(3, 163);
+            btnRight.Name = "btnRight";
+            btnRight.Size = new Size(38, 44);
+            btnRight.TabIndex = 0;
+            btnRight.Text = "▶";
+            btnRight.UseVisualStyleBackColor = true;
+            btnRight.Click += btnRight_Click;
+            // 
+            // tableLayoutPanelBtns
+            // 
+            tableLayoutPanelBtns.ColumnCount = 4;
+            tableLayoutPanelBtns.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 150F));
+            tableLayoutPanelBtns.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanelBtns.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanelBtns.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanelBtns.Controls.Add(panelTimer, 0, 0);
+            tableLayoutPanelBtns.Controls.Add(panelStartBtn, 1, 0);
+            tableLayoutPanelBtns.Controls.Add(panelPause, 2, 0);
+            tableLayoutPanelBtns.Controls.Add(panelStop, 3, 0);
+            tableLayoutPanelBtns.Dock = DockStyle.Fill;
+            tableLayoutPanelBtns.Location = new Point(3, 386);
+            tableLayoutPanelBtns.Name = "tableLayoutPanelBtns";
+            tableLayoutPanelBtns.RowCount = 1;
+            tableLayoutPanelBtns.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanelBtns.Size = new Size(794, 37);
+            tableLayoutPanelBtns.TabIndex = 1;
+            // 
+            // panelTimer
+            // 
+            panelTimer.Controls.Add(label2);
+            panelTimer.Controls.Add(label1);
+            panelTimer.Controls.Add(textBoxSec);
+            panelTimer.Controls.Add(textBoxMin);
+            panelTimer.Controls.Add(textBoxHour);
+            panelTimer.Dock = DockStyle.Fill;
+            panelTimer.Location = new Point(3, 3);
+            panelTimer.Name = "panelTimer";
+            panelTimer.Size = new Size(144, 31);
+            panelTimer.TabIndex = 0;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(88, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(10, 15);
+            label2.TabIndex = 1;
+            label2.Text = ":";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(45, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(10, 15);
+            label1.TabIndex = 1;
+            label1.Text = ":";
+            // 
+            // textBoxSec
+            // 
+            textBoxSec.Location = new Point(104, 4);
+            textBoxSec.Name = "textBoxSec";
+            textBoxSec.Size = new Size(23, 23);
+            textBoxSec.TabIndex = 0;
+            textBoxSec.Text = "0";
+            textBoxSec.KeyPress += textBoxSec_KeyPress;
+            // 
+            // textBoxMin
+            // 
+            textBoxMin.Location = new Point(60, 4);
+            textBoxMin.Name = "textBoxMin";
+            textBoxMin.Size = new Size(23, 23);
+            textBoxMin.TabIndex = 0;
+            textBoxMin.Text = "0";
+            textBoxMin.KeyPress += textBoxMin_KeyPress;
+            // 
+            // textBoxHour
+            // 
+            textBoxHour.Location = new Point(16, 4);
+            textBoxHour.Name = "textBoxHour";
+            textBoxHour.Size = new Size(23, 23);
+            textBoxHour.TabIndex = 0;
+            textBoxHour.Text = "0";
+            textBoxHour.KeyPress += textBoxHour_KeyPress;
+            // 
+            // panelStartBtn
+            // 
+            panelStartBtn.Controls.Add(btnStart);
+            panelStartBtn.Dock = DockStyle.Fill;
+            panelStartBtn.Location = new Point(153, 3);
+            panelStartBtn.Name = "panelStartBtn";
+            panelStartBtn.Size = new Size(208, 31);
+            panelStartBtn.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            btnStart.Dock = DockStyle.Fill;
+            btnStart.Location = new Point(0, 0);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(208, 31);
+            btnStart.TabIndex = 0;
+            btnStart.Text = "▶";
+            btnStart.UseVisualStyleBackColor = true;
+            btnStart.Click += btnStart_Click;
+            // 
+            // panelPause
+            // 
+            panelPause.Controls.Add(btnPause);
+            panelPause.Dock = DockStyle.Fill;
+            panelPause.Location = new Point(367, 3);
+            panelPause.Name = "panelPause";
+            panelPause.Size = new Size(208, 31);
+            panelPause.TabIndex = 2;
+            // 
+            // btnPause
+            // 
+            btnPause.Dock = DockStyle.Fill;
+            btnPause.Location = new Point(0, 0);
+            btnPause.Name = "btnPause";
+            btnPause.Size = new Size(208, 31);
+            btnPause.TabIndex = 0;
+            btnPause.Text = "❚❚";
+            btnPause.UseVisualStyleBackColor = true;
+            btnPause.Click += btnPause_Click;
+            // 
+            // panelStop
+            // 
+            panelStop.Controls.Add(btnStop);
+            panelStop.Dock = DockStyle.Fill;
+            panelStop.Location = new Point(581, 3);
+            panelStop.Name = "panelStop";
+            panelStop.Size = new Size(210, 31);
+            panelStop.TabIndex = 3;
+            // 
+            // btnStop
+            // 
+            btnStop.Dock = DockStyle.Fill;
+            btnStop.Location = new Point(0, 0);
+            btnStop.Name = "btnStop";
+            btnStop.Size = new Size(210, 31);
+            btnStop.TabIndex = 0;
+            btnStop.Text = "■";
+            btnStop.UseVisualStyleBackColor = true;
+            btnStop.Click += btnStop_Click;
+            // 
+            // menuStrip
+            // 
+            menuStrip.Items.AddRange(new ToolStripItem[] { loadToolStripMenuItem });
+            menuStrip.Location = new Point(0, 0);
+            menuStrip.Name = "menuStrip";
+            menuStrip.Size = new Size(800, 24);
+            menuStrip.TabIndex = 1;
+            menuStrip.Text = "menuStrip1";
+            // 
+            // loadToolStripMenuItem
+            // 
+            loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            loadToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.L;
+            loadToolStripMenuItem.Size = new Size(45, 20);
+            loadToolStripMenuItem.Text = "Load";
+            loadToolStripMenuItem.Click += loadToolStripMenuItem_Click;
+            // 
+            // timer
+            // 
+            timer.Interval = 1;
+            timer.Tick += timer_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tableLayoutPanelForm);
+            Controls.Add(menuStrip);
+            MainMenuStrip = menuStrip;
+            Name = "Form1";
+            Text = "Form1";
+            SizeChanged += Form1_SizeChanged;
+            tableLayoutPanelForm.ResumeLayout(false);
+            tableLayoutPanelPicBox.ResumeLayout(false);
+            panelPicBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)picBox).EndInit();
+            tableLayoutPanelLeftBtn.ResumeLayout(false);
+            panelCheckBox.ResumeLayout(false);
+            panelCheckBox.PerformLayout();
+            tableLayoutPanelRightBtn.ResumeLayout(false);
+            tableLayoutPanelBtns.ResumeLayout(false);
+            panelTimer.ResumeLayout(false);
+            panelTimer.PerformLayout();
+            panelStartBtn.ResumeLayout(false);
+            panelPause.ResumeLayout(false);
+            panelStop.ResumeLayout(false);
+            menuStrip.ResumeLayout(false);
+            menuStrip.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanelForm;
+        private MenuStrip menuStrip;
+        private ToolStripMenuItem loadToolStripMenuItem;
+        private TableLayoutPanel tableLayoutPanelPicBox;
+        private TableLayoutPanel tableLayoutPanelBtns;
+        private Panel panelTimer;
+        private Panel panelStartBtn;
+        private Panel panelPause;
+        private Panel panelStop;
+        private Panel panelPicBox;
+        private TableLayoutPanel tableLayoutPanelLeftBtn;
+        private Button btnLeft;
+        private TableLayoutPanel tableLayoutPanelRightBtn;
+        private Button btnRight;
+        private PictureBox picBox;
+        private Button btnStart;
+        private Button btnPause;
+        private Button btnStop;
+        private TextBox textBoxHour;
+        private TextBox textBoxSec;
+        private TextBox textBoxMin;
+        private Label label1;
+        private Label label2;
+        private System.Windows.Forms.Timer timer;
+        private Panel panelCheckBox;
+        private CheckBox checkBoxSize;
+        private Label label3;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,204 @@
+using System.Windows.Forms;
+
+namespace Croquis
+{
+    public partial class Form1 : Form
+    {
+        List<Bitmap> bitmapsList = new List<Bitmap>();
+        int idx = 0;
+        int hour = 0;
+        int min = 0;
+        int sec = 0;
+        bool timerFlag = false;
+        bool sizeToggled = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            if (this.Width <= 300) this.Width = 300;
+            if (this.Height <= 300) this.Height = 300;
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "( *.bmp; *.jpg; *.png; *.jpeg) | *.BMP; *.JPG; *.PNG; *.JPEG";
+            open.Multiselect = true;
+            if (open.ShowDialog() == DialogResult.OK && open.FileNames.Length > 0)
+            {
+                for (int i = 0; i < open.FileNames.Length; i++)
+                {
+                    bitmapsList.Add(new Bitmap(open.FileNames[i]));
+                }
+                picBox.Image = bitmapsList[bitmapsList.Count - 1];
+            }
+        }
+
+        private void btnLeft_Click(object sender, EventArgs e)
+        {
+            if (idx == 0)
+            {
+                idx = bitmapsList.Count - 1;
+                picBox.Image = bitmapsList[idx];
+            }
+            else
+            {
+                picBox.Image = bitmapsList[--idx];
+            }
+        }
+
+        private void btnRight_Click(object sender, EventArgs e)
+        {
+            if (idx == bitmapsList.Count - 1)
+            {
+                idx = 0;
+                picBox.Image = bitmapsList[idx];
+            }
+            else
+            {
+                picBox.Image = bitmapsList[++idx];
+            }
+        }
+
+        private void textBoxHour_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsDigit(e.KeyChar)
+                || e.KeyChar == Convert.ToChar(Keys.Back)
+                || e.KeyChar == Convert.ToChar(Keys.Delete)
+                ))
+            {
+                e.Handled = true;
+            }
+            hour = int.Parse(textBoxHour.Text);
+        }
+
+        private void textBoxMin_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsDigit(e.KeyChar)
+                || e.KeyChar == Convert.ToChar(Keys.Back)
+                || e.KeyChar == Convert.ToChar(Keys.Delete)
+                ))
+            {
+                e.Handled = true;
+            }
+            min = int.Parse(textBoxMin.Text);
+        }
+
+        private void textBoxSec_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsDigit(e.KeyChar)
+                || e.KeyChar == Convert.ToChar(Keys.Back)
+                || e.KeyChar == Convert.ToChar(Keys.Delete)
+                ))
+            {
+                e.Handled = true;
+            }
+            sec = int.Parse(textBoxSec.Text);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timerFlag = true;
+            timer.Enabled = true;
+            timer.Start();
+            // TryParse로 입력값을 안전하게 변환
+            if (!int.TryParse(textBoxHour.Text, out hour)) hour = 0;
+            if (!int.TryParse(textBoxMin.Text, out min)) min = 0;
+            if (!int.TryParse(textBoxSec.Text, out sec)) sec = 0;
+
+            // 시간과 분은 60 이상이 될 수 없도록 체크
+            if (min >= 60 || sec >= 60)
+            {
+                MessageBox.Show("시간과 분은 60 이상일 수 없습니다.", "잘못된 입력", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            if (timerFlag == false)
+            {
+                timerFlag = true;
+                timer.Start();
+            }
+            else
+            {
+                timerFlag = false;
+                timer.Stop();
+            }
+        }
+
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            hour = 0;
+            min = 0;
+            sec = 0;
+
+            textBoxHour.Text = hour.ToString();
+            textBoxMin.Text = min.ToString();
+            textBoxSec.Text = sec.ToString();
+
+            timerFlag = false;
+            timer.Stop();
+            timer.Enabled = false;
+        }
+
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            sec--;
+
+            if (hour == 0 && min == 0 && sec == 0)
+            {
+                sec = 0;
+                timerFlag = false;
+                timer.Stop();
+                timer.Enabled = false;
+                if (idx == bitmapsList.Count - 1)
+                {
+                    idx = 0;
+                    picBox.Image = bitmapsList[idx];
+                }
+                else
+                {
+                    idx++;
+                    picBox.Image = bitmapsList[idx];
+                }
+            }
+            else if (sec == 0)
+            {
+                if (hour != 0 && min == 0)
+                {
+                    hour--;
+                    min = 59;
+                }
+                else
+                {
+                    min--;
+                }
+                sec = 59;
+            }
+
+            textBoxHour.Text = hour.ToString();
+            textBoxMin.Text = min.ToString();
+            textBoxSec.Text = sec.ToString();
+        }
+
+        private void checkBoxSize_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxSize.Checked)
+            {
+                picBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else
+            {
+                picBox.SizeMode = PictureBoxSizeMode.Normal;
+            }
+        }
+    }
+}
